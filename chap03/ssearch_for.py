980cc07b5af4049ee6d79597cad2a9925e2d4f4f
qotjdwlr1029 <qotjdwlrdl@gmail.com>
2022-06-07 23:45:41 +0900

    chap03 search algorithm

---
+++ chap03/ssearch_for.py
@@ -0,0 +1,9 @@
+# for 문으로 작성한 선형 검색 알고리즘
+
+from typing import Any, Sequence
+
+def seq_search(a: Sequence, key: Any ) -> int:
+    for i in range(len(a)):
+        if a[i] == key:
+            return i
+    return -1
